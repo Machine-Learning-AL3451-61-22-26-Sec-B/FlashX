00424e140329dd5559af44fd2381123274860150
Tarun Kumar S A <164351255+tarunkumar22aib51@users.noreply.github.com>
2024-05-29 10:06:31 +0530

    Update 2.py

--- Week2/app.py
+++ Week2/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sklearn
-
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
